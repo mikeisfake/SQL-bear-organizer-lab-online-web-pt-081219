c337c285e298032e5fc2ae08f2c8177ef29a45cb
Mike Cooper <mikescooper@icloud.com>
2019-10-15 20:55:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES 
-("Mr. Chocolate", 13,"female", "brown","smooth",1),
-("Rowdy", 10,"male", "brown","rowdy",1),
-("Tabitha",, 25,"female", "black","motherly",1),
-("Sergeant Brown", 28,"female", "gray","authoritative",1),
-("Melissa", 20,"female", "white","friendly",1),
-("Grinch", 273,"male", "green","heartless",1),
-("Wendy", 6,"female", "yellow","shes baby",1),
-(NULL, 29,"male", "white","pure evil",0);
+("Mr. Chocolate", 13,"female", "brown", "smooth", 1),
+("Rowdy", 10, "male", "brown","rowdy", 1),
+("Tabitha", 25, "female", "black","motherly", 1),
+("Sergeant Brown", 28, "female", "gray", "authoritative", 1),
+("Melissa", 20, "female", "white", "friendly", 1),
+("Grinch", 273, "male", "green", "heartless", 1),
+("Wendy", 6, "female", "yellow", "shes baby", 1),
+(NULL, 29, "male", "white", "pure evil", 0);
