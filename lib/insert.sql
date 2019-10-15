30e0e2873f33b50239f4128cba38502eefb21288
Mike Cooper <mikescooper@icloud.com>
2019-10-15 20:44:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy");
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha");
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ();
